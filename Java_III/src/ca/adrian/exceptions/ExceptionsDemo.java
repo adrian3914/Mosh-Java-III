b844f34cefebb340050d87e85a617fc914daccba
Adrian Paz Alonso <adrian.pa31@outlook.com>
2022-01-13 13:57:19 -0500

    handling multiple exceptions

--- Java_III/src/ca/adrian/exceptions/ExceptionsDemo.java
+++ Java_III/src/ca/adrian/exceptions/ExceptionsDemo.java
@@ -2,15 +2,19 @@ package ca.adrian.exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
 
     public static void show(){
         try {
             var reader = new FileReader("hello.txt");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            var value  = reader.read();
+            new SimpleDateFormat().parse("");
+        } catch (IOException | ParseException e) {
+            System.out.println("Could not read data.");
         }
     }
-
 }
