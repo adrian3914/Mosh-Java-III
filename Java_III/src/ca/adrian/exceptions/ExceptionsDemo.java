3061480c6f3e87cfa944f600f7f0597bee890080
Adrian Paz Alonso <adrian.pa31@outlook.com>
2022-01-13 13:30:57 -0500

    try and catch block added to ca.adrian.ExceptionsDemo.show

--- Java_III/src/ca/adrian/exceptions/ExceptionsDemo.java
+++ Java_III/src/ca/adrian/exceptions/ExceptionsDemo.java
@@ -1,12 +1,16 @@
 package ca.adrian.exceptions;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class ExceptionsDemo {
 
     public static void show(){
-        sayHello(null);
+        try {
+            var reader = new FileReader("hello.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void sayHello(String name){
-        System.out.println(name.toUpperCase());
-    }
 }
