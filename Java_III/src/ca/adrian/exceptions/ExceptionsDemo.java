aacb5f72e76d46250243c258c8c36596acc36bc7
Adrian Paz Alonso <adrian.pa31@outlook.com>
2022-01-13 14:05:33 -0500

    Using finally keyword

--- Java_III/src/ca/adrian/exceptions/ExceptionsDemo.java
+++ Java_III/src/ca/adrian/exceptions/ExceptionsDemo.java
@@ -9,12 +9,24 @@ import java.text.SimpleDateFormat;
 public class ExceptionsDemo {
 
     public static void show(){
+        FileReader reader = null;
+
         try {
-            var reader = new FileReader("hello.txt");
+            reader = new FileReader("hello.txt");
             var value  = reader.read();
             new SimpleDateFormat().parse("");
-        } catch (IOException | ParseException e) {
+        }
+        catch (IOException | ParseException e) {
             System.out.println("Could not read data.");
+        }
+        finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
