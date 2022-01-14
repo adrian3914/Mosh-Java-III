4ad0253d319c32af6bf31a6c2f7d5ecd190e1980
Adrian Paz Alonso <adrian.pa31@outlook.com>
2022-01-14 08:32:41 -0500

    changed int[] to objc[], in order to pass any data type - POOR SOLUTION -

--- Java_III/src/ca/adrian/Main.java
+++ Java_III/src/ca/adrian/Main.java
@@ -1,13 +1,20 @@
 package ca.adrian;
 
-import ca.adrian.exceptions.Account;
-import ca.adrian.exceptions.AccountException;
+
 import ca.adrian.generics.List;
+import ca.adrian.generics.User;
 
 public class Main {
 
     public static void main(String[] args) {
       var list = new List();
+      list.add(Integer.valueOf(1));// we can pass primitive type cause the compiler will use its wrapper class and implicit cast it to its wrapper
+      list.add("1");
+      list.add(new User());
+
+      // Problems with this implementation
+        int number = (int) list.get(0);// must cast it to desired data type
+        int number2 = (int) list.get(1);// We will get a ClassCastException if we provide the wrong data type
 
     }
 }
