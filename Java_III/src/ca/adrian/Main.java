788816d397c6f4b5eb69c9ef71cbaf202ea5351b
Adrian Paz Alonso <adrian.pa31@outlook.com>
2022-01-18 12:42:52 -0500

    Initializing Interface passing a lambda expression

--- Java_III/src/ca/adrian/Main.java
+++ Java_III/src/ca/adrian/Main.java
@@ -12,7 +12,7 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        //LambdasDemo.show();
+        LambdasDemo.show();
 
     }
 }
--- Java_III/src/ca/adrian/lambdas/LambdasDemo.java
+++ Java_III/src/ca/adrian/lambdas/LambdasDemo.java
@@ -1,11 +1,16 @@
 package ca.adrian.lambdas;
 
 public class LambdasDemo {
-    String  prefix = "-";
 
-    public void show(){
+
+    public static void show(){
+        String  prefix = "-";
         // Passing Lambda expression as an argument to a method
-        greet(message -> System.out.println(this + message));
+        //greet(message -> System.out.println(this + message));
+
+        // Initializing Variable Interface
+        Printer printer = message -> System.out.println(message);
+        printer.print("Hello!");
     }
 
     public static void greet(Printer printer){
