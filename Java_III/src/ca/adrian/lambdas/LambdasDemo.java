7bc59a0263fb742a92257c634858cc0ba3ea9af9
Adrian Paz Alonso <adrian.pa31@outlook.com>
2022-01-18 12:39:35 -0500

    Lambda expression

--- Java_III/src/ca/adrian/lambdas/LambdasDemo.java
+++ Java_III/src/ca/adrian/lambdas/LambdasDemo.java
@@ -1,17 +1,24 @@
 package ca.adrian.lambdas;
 
 public class LambdasDemo {
+    String  prefix = "-";
 
-    public static void show(){
-        greet(new Printer() {
-            @Override
-            public void print(String message) {
-                System.out.println(message);
-            }
-        });
+    public void show(){
+        // Passing Lambda expression as an argument to a method
+        greet(message -> System.out.println(this + message));
     }
 
     public static void greet(Printer printer){
         printer.print("Hello World!");
     }
 }
+
+/*
+    Anonymous class to implement interface in the function without the need of creating a class that implements it.
+    greet(new Printer() {
+            @Override
+            public void print(String message) {
+                System.out.println(message);
+            }
+        });
+ */
