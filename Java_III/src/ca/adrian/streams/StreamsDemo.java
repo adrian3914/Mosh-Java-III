f2bce755c26fbae1e63f491521a9fe319c1e2d8e
Adrian Paz Alonso <adrian.pa31@outlook.com>
2022-01-20 09:08:23 -0500

    Creating Streams from Collection, Array, Arbitrary number of objects and creating infinive streams

--- Java_III/src/ca/adrian/streams/StreamsDemo.java
+++ Java_III/src/ca/adrian/streams/StreamsDemo.java
@@ -1,9 +1,42 @@
 package ca.adrian.streams;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class StreamsDemo {
     public static void show(){
+        // CREATING STREAMS
+        // Collections
+        Collection<Integer> x = List.of(1, 2, 3);
+        x.stream();
+
+        // Arrays
+        int[] numbers = {1, 2, 3};
+        Arrays.stream(numbers)
+                .forEach(n -> System.out.println(n));
+
+        // Arbitrary numbers of objects
+        Stream.of(1, 2, 3, 4)
+                .count();
+
+        // Infinitive Streams
+        var stream  = Stream.generate(() -> Math.random()); // Lazy Evaluation nothing is generated until is called
+        stream
+                .limit(3) // prevent to be an infinitive loop
+                .forEach(n -> System.out.println(n));
+
+        // iterate()
+        Stream.iterate(1, n -> n + 1)
+                .limit(10)
+                .forEach(n -> System.out.println(n));
+    }
+}
+
+/*
+    // Streams
 
         List<Movie> movies = List.of(
                 new Movie("a", 10),
@@ -24,5 +57,4 @@ public class StreamsDemo {
                 .filter(movie -> movie.getLikes() > 10)
                 .count();
 
-    }
-}
+ */
