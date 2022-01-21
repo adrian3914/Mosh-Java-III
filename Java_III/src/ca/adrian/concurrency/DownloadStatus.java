ea6e7cf33fdbb598050927fc1318904e19f38c2f
Adrian Paz Alonso <adrian.pa31@outlook.com>
2022-01-21 16:20:33 -0500

    Locks - Thread safe

--- Java_III/src/ca/adrian/concurrency/DownloadStatus.java
+++ Java_III/src/ca/adrian/concurrency/DownloadStatus.java
@@ -1,13 +1,23 @@
 package ca.adrian.concurrency;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class DownloadStatus {
     private int totalBytes;
+    private Lock lock = new ReentrantLock();
 
     public int getTotalBytes() {
         return totalBytes;
     }
 
     public void incrementTotalBytes(){
-        totalBytes++;
+        lock.lock();
+        try {
+            totalBytes++;
+        }
+        finally {
+            lock.unlock(); // always put unlock in the finally block in case there is an exception, so we can unlock
+        }
     }
 }
