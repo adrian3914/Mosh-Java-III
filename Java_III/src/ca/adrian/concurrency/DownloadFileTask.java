7f426dddf5221115dc7566d45acf0ba81a44dc3a
Adrian Paz Alonso <adrian.pa31@outlook.com>
2022-01-21 09:32:20 -0500

    Pausing a thread Thread.sleep(5000) 5 secs

--- Java_III/src/ca/adrian/concurrency/DownloadFileTask.java
+++ Java_III/src/ca/adrian/concurrency/DownloadFileTask.java
@@ -4,5 +4,14 @@ public class DownloadFileTask implements Runnable{
     @Override
     public void run() {
         System.out.println("Downloading a file: " + Thread.currentThread().getName());
+
+        // pausing the thread .sleep()
+        try {
+            Thread.sleep(5000); // 5sec
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Download complete: " + Thread.currentThread().getName());
     }
 }
--- Java_III/src/ca/adrian/concurrency/ThreadDemo.java
+++ Java_III/src/ca/adrian/concurrency/ThreadDemo.java
@@ -4,7 +4,6 @@ public class ThreadDemo {
     public static void show(){
         System.out.println(Thread.currentThread().getName());
 
-
         for (int i = 0; i < 10; i++){
             // CREATE A THREAD
             Thread thread = new Thread(new DownloadFileTask());
