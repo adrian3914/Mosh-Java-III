f304f05f805d439c750be5c1a2d39cea3397e752
Adrian Paz Alonso <adrian.pa31@outlook.com>
2022-01-21 10:34:45 -0500

    Joining threads, join() current thread waits for the completion of another thread

--- Java_III/src/ca/adrian/concurrency/ThreadDemo.java
+++ Java_III/src/ca/adrian/concurrency/ThreadDemo.java
@@ -2,13 +2,27 @@ package ca.adrian.concurrency;
 
 public class ThreadDemo {
     public static void show(){
-        System.out.println(Thread.currentThread().getName());
+        // Downloading a file
+       Thread thread = new Thread(new DownloadFileTask());
+       thread.start();
+
+       // We want to scan for virus after download is complete in a separate thread
+        try {
+            // we tell the current thread to wait for the completion of another thread, while waiting it cannot do other things
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("File is ready to be scanned");
+    }
+}
+
+/*
+     System.out.println(Thread.currentThread().getName());
 
         for (int i = 0; i < 10; i++){
             // CREATE A THREAD
             Thread thread = new Thread(new DownloadFileTask());
             thread.start();
         }
-
-    }
-}
+ */
