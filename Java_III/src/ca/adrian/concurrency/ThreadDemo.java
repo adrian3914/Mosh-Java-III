2fd1ca69abd428d3550752262b95985a19e66815
Adrian Paz Alonso <adrian.pa31@outlook.com>
2022-01-21 09:24:10 -0500

    Starting 10 threads

--- Java_III/src/ca/adrian/concurrency/ThreadDemo.java
+++ Java_III/src/ca/adrian/concurrency/ThreadDemo.java
@@ -4,8 +4,12 @@ public class ThreadDemo {
     public static void show(){
         System.out.println(Thread.currentThread().getName());
 
-        // CREATE A THREAD
-        Thread thread = new Thread(new DownloadFileTask());
-        thread.start();
+
+        for (int i = 0; i < 10; i++){
+            // CREATE A THREAD
+            Thread thread = new Thread(new DownloadFileTask());
+            thread.start();
+        }
+
     }
 }
