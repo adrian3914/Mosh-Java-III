d4f32c4eff90ba1b9451dc83a98022aa86eb3201
Adrian Paz Alonso <adrian.pa31@outlook.com>
2022-01-26 10:04:19 -0500

    Callable and futures, passing a task that will be done by another thread and current thread continues

--- Java_III/src/ca/adrian/executors/ExecutorsDemo.java
+++ Java_III/src/ca/adrian/executors/ExecutorsDemo.java
@@ -1,29 +1,45 @@
 package ca.adrian.executors;
 
-import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
 
 public class ExecutorsDemo {
-
     public static void show(){
+        // CALLABLE AND FUTURES
+        var executor  = Executors.newFixedThreadPool(2);
+
+        try {
+            // passing a future task -> it will take sometime to get result
+            var future = executor.submit(() -> {
+                LongTask.simulate();
+                return 1;
+            });
+
+            System.out.println("Do more work");
+            var result = future.get(); // Main thread has to wait until the result is ready, block method
+            System.out.println(result);
+
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            executor.shutdown();
+        }
+
+    }
+}
+
+/*
         // CREATE A THREAD POOL -> Using Executors static factory methods
         var executor = Executors.newFixedThreadPool(2);
-        try {
-            // checking what class is instantiate at runTime
-            System.out.println(executor.getClass().getName());
-
-            // Passing a task
+    try {
+            // Passing a task to the thread pool
             executor.submit(() ->{
                 // print the name of the current thread
                 System.out.println(Thread.currentThread().getName());
             });
-        } catch (Exception e) {
-            e.printStackTrace();
         }
         finally {
             // CLOSE EXECUTOR TO TERMINATE THE PROGRAM
             executor.shutdown();
         }
-    }
-}
+ */
