f8d9056613ae25defc1d2930f9864d66f69ce283
Adrian Paz Alonso <adrian.pa31@outlook.com>
2022-01-26 11:34:48 -0500

    Creating a completable future, runAsync(task) does not return a value

--- Java_III/src/ca/adrian/executors/ExecutorsDemo.java
+++ Java_III/src/ca/adrian/executors/ExecutorsDemo.java
@@ -1,29 +1,15 @@
 package ca.adrian.executors;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ForkJoinPool;
 
 public class ExecutorsDemo {
     public static void show(){
-        // CALLABLE AND FUTURES
-        var executor  = Executors.newFixedThreadPool(2);
-
-        try {
-            // passing a future task -> it will take sometime to get result
-            var future = executor.submit(() -> {
-                LongTask.simulate();
-                return 1;
-            });
-
-            System.out.println("Do more work");
-            var result = future.get(); // Main thread has to wait until the result is ready, block method
-            System.out.println(result);
-
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            executor.shutdown();
-        }
+        // CREATING A COMPLETABLE FUTURE
+        Runnable task  = () -> System.out.println("a");
+        var future = CompletableFuture.runAsync(task);
 
     }
 }
@@ -40,6 +26,28 @@ public class ExecutorsDemo {
         }
         finally {
             // CLOSE EXECUTOR TO TERMINATE THE PROGRAM
+            executor.shutdown();
+        }
+ */
+
+/*
+    // CALLABLE AND FUTURES
+        var executor  = Executors.newFixedThreadPool(2);
+
+        try {
+            // passing a future task -> it will take sometime to get result
+            var future = executor.submit(() -> {
+                LongTask.simulate();
+                return 1;
+            });
+
+            System.out.println("Do more work");
+            var result = future.get(); // Main thread has to wait until the result is ready, block method
+            System.out.println(result);
+
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
             executor.shutdown();
         }
  */
