ffb7e618e99c08333e76f14aa394122db4000a3f
Adrian Paz Alonso <adrian.pa31@outlook.com>
2022-04-11 08:27:00 -0400

    Try with Resources Statement

--- Java_III_Second_Time/src/ca/adrian/exceptions/ExceptionsDemo.java
+++ Java_III_Second_Time/src/ca/adrian/exceptions/ExceptionsDemo.java
@@ -1,27 +1,19 @@
 package ca.adrian.exceptions;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+
 
 public class ExceptionsDemo {
     public static void show(){
-        FileReader reader = null;
-        try {
-            reader = new FileReader("file.txt");
+        try (
+                var reader = new FileReader("file.txt");
+                var writer = new FileWriter("...");
+        ){
             var value = reader.read();
         } catch (IOException e) {
             e.printStackTrace();
-        }finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 
