0cec36391939674441c62749b4bc704f5ea0fc94
Adrian Paz Alonso <adrian.pa31@outlook.com>
2022-04-11 08:51:00 -0400

    Creating custom exception

--- Java_III_Second_Time/src/ca/adrian/exceptions/Account.java
+++ Java_III_Second_Time/src/ca/adrian/exceptions/Account.java
@@ -3,9 +3,17 @@ package ca.adrian.exceptions;
 import java.io.IOException;
 
 public class Account {
+    private float balance;
+
     public void deposit(float value) throws IOException{
         if (value <= 0) {
             throw  new IOException();
+        }
+    }
+
+    public void withdraw(float value) throws InsufficientFundsException{
+        if (value > balance){
+            throw new InsufficientFundsException();
         }
     }
 }
