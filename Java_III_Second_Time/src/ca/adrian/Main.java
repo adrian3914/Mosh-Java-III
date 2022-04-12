7dfe1608df27cf4ee03f3d6b323da9d5ef78ce5b
Adrian Paz Alonso <adrian.pa31@outlook.com>
2022-04-12 07:43:40 -0400

    Generics and primitive types

--- Java_III_Second_Time/src/ca/adrian/Main.java
+++ Java_III_Second_Time/src/ca/adrian/Main.java
@@ -1,17 +1,16 @@
 package ca.adrian;
 
-import ca.adrian.exceptions.ExceptionsDemo;
-
-import java.io.IOException;
+import ca.adrian.generics.GenericList;
 
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            ExceptionsDemo.show();
-        } catch (IOException e) {
-            System.out.println("An unexpected error occurred.");
-            e.printStackTrace();
-        }
+        // int -> Integer
+        // float -> Float
+        // boolean -> Boolean
+
+        GenericList<Integer> numbers = new GenericList<>();
+        numbers.add(1); // Boxing
+        int number = numbers.get(0);// Unboxing
     }
 }
