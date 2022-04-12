5cc5d377a05413526fc0fc8339a8c63b6107bdf7
Adrian Paz Alonso <adrian.pa31@outlook.com>
2022-04-12 07:58:32 -0400

    Adding constraint to type parameters

--- Java_III_Second_Time/src/ca/adrian/Main.java
+++ Java_III_Second_Time/src/ca/adrian/Main.java
@@ -1,16 +1,11 @@
 package ca.adrian;
 
 import ca.adrian.generics.GenericList;
+import ca.adrian.generics.User;
 
 public class Main {
-
     public static void main(String[] args) {
-        // int -> Integer
-        // float -> Float
-        // boolean -> Boolean
+        GenericList<User> numbers = new GenericList<>();
 
-        GenericList<Integer> numbers = new GenericList<>();
-        numbers.add(1); // Boxing
-        int number = numbers.get(0);// Unboxing
     }
 }
--- Java_III_Second_Time/src/ca/adrian/generics/User.java
+++ Java_III_Second_Time/src/ca/adrian/generics/User.java
@@ -1,4 +1,8 @@
 package ca.adrian.generics;
 
-public class User {
+public class User implements Comparable, Cloneable{
+    @Override
+    public int compareTo(Object o) {
+        return 0;
+    }
 }
