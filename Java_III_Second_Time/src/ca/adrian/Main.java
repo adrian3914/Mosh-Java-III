9f8a0b3b8926f3582a0aa4a60973a00d32dc5a8e
Adrian Paz Alonso <adrian.pa31@outlook.com>
2022-04-13 10:55:44 -0400

    Utils class implemented with generic method

--- Java_III_Second_Time/src/ca/adrian/Main.java
+++ Java_III_Second_Time/src/ca/adrian/Main.java
@@ -1,19 +1,13 @@
 package ca.adrian;
 
 import ca.adrian.generics.User;
+import ca.adrian.generics.Utils;
 
 public class Main {
     public static void main(String[] args) {
-       var user1 = new User(20);
-       var user2 = new User(40);
-
-       var result = user1.compareTo(user2);
-
-       if (result < 0)
-           System.out.println("User1 < User2");
-       else if (result == 0)
-           System.out.println("User1 == User2");
-       else
-           System.out.println("User1 > User2");
+        var max = Utils.max(new User(10), new User(20));
+        System.out.println(max);
     }
+
+
 }
